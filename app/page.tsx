03b9b15f909c1bedb192ae04dc7bdc391bb19c47
SyedJunaidAli1 <syedjunaidali790@gmail.com>
2026-04-04 13:56:26 +0530

    redirect added in route

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,5 @@
-
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  return (
-    <div className="flex flex-col font-sans">
-    
-    </div>
-  );
+  redirect("/user");
 }
